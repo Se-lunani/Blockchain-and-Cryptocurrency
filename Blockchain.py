abbea9def357982b84efa75be2d8fcb90fdab512
Se-lunani <mwamiselunani@gmail.com>
2019-06-20 17:20:30 +0300

    A start of a blockchain and cryptocurrency project.

---
+++ Blockchain.py
@@ -0,0 +1,27 @@
+blockchain = []
+
+
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+
+def add_value(transaction_amount, last_transaction=None):
+    if last_transaction is None:
+        last_transaction = [1]
+    blockchain.append([last_transaction, transaction_amount])
+
+
+def get_user_input():
+    return float(input('Enter your Transaction Amount PLease:'))
+
+
+tx_amount = get_user_input()
+add_value(tx_amount)
+tx_amount = get_user_input()
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
+tx_amount = get_user_input()
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
+tx_amount = get_user_input()
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
+
+print(blockchain)
