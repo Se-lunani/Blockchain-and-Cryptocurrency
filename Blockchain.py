4ddc1703e0a3f95487e39a7030962a1e7ce216db
Se-lunani <mwamiselunani@gmail.com>
2019-06-28 19:28:14 +0300

    using your own imports

--- Blockchain.py
+++ Blockchain.py
@@ -1,7 +1,8 @@
 from functools import  reduce
 import hashlib as hl
-import json
 from collections import OrderedDict
+
+from hash_util import hash_string_256, hash_block
 
 # initializing the blockchain list
 MINING_REWARD = 10
@@ -18,12 +19,10 @@ owner = 'Evan'
 participants = {'Evan'}
 
 
-def hash_block(block):
-    return hl.sha256(json.dumps(block, sort_keys= True).encode()).hexdigest()
 
 def valid_proof(transactions, last_hash, proof):
     guess = (str(transactions) + str(last_hash) + str(proof)).encode()
-    guess_hash = hl.sha256(guess).hexdigest()
+    guess_hash = hash_string_256(guess)
     print(guess_hash)
 
     return guess_hash[0:2] == '00'
