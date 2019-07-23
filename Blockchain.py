8bfa0cc6f9917343eed6540119b1ed15dd796bdb
Se-lunani <mwamiselunani@gmail.com>
2019-07-23 14:38:16 +0300

    Handling index error and Json formats

--- Blockchain.py
+++ Blockchain.py
@@ -55,7 +55,7 @@ def load_data():
                     [('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])])
                 updated_transactions.append(updated_transaction)
             open_transactions = updated_transactions
-    except IOError:
+    except (IOError, IndexError):
         # Our starting block for the blockchain
         genesis_block = Block(0,'',[],100,0)
         # Initializing our (empty) blockchain list
@@ -73,7 +73,8 @@ def save_data():
     """Save blockchain + open transactions snapshot to a file."""
     try:
         with open('blockchain.txt', mode='w') as f:
-            f.write(json.dumps(blockchain))
+            savable_chain = [block.__dict__ for block in blockchain]
+            f.write(json.dumps(savable_chain))
             f.write('\n')
             f.write(json.dumps(open_transactions))
             # save_data = {
--- hash_util.py
+++ hash_util.py
@@ -5,4 +5,5 @@ def hash_string_256(string):
     return hl.sha256(string).hexdigest()
 
 def hash_block(block):
+    hashable_block = block.__dict__.copy()
     return hl.sha256(json.dumps(block, sort_keys= True).encode()).hexdigest()
