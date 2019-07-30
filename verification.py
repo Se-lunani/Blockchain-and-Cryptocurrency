f6f123a83365fc688f15ec1144649c586c3a3b85
Se-lunani <mwamiselunani@gmail.com>
2019-07-30 14:16:41 +0300

    using class methods

--- verification.py
+++ verification.py
@@ -18,7 +18,7 @@ class Verification:
         # Only a hash (which is based on the above inputs) which starts with two 0s is treated as valid
         # This condition is of course defined by you. You could also require 10 leading 0s - this would take significantly longer (and this allows you to control the speed at which new blocks can be added)
         return guess_hash[0:2] == '00'
-
+    @classmethod
     def verify_chain(self, blockchain):
         """ Verify the current blockchain and return True if it's valid, False otherwise."""
         for (index, block) in enumerate(blockchain):
