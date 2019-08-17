19415299b570e121a05dd0cb7f7fdbe5d3df2d88
Mwami Se-lunani <mwamiselunani@gmail.com>
2019-08-17 11:55:22 +0300

    Signature for creating a new transaction

--- wallet.py
+++ wallet.py
@@ -1,6 +1,9 @@
 from Crypto.PublicKey import RSA
+from Crypto.Signature import  PKCS1_v1_5
+from Crypto.Hash import SHA256
 import Crypto.Random
 import binascii
+
 
 class Wallet:
     def __init__(self):
@@ -40,3 +43,9 @@ class Wallet:
         private_key = RSA.generate(1024, Crypto.Random.new().read)
         public_key = private_key.publickey()
         return (binascii.hexlify(private_key.exportKey(format='DER')).decode('ascii'),binascii.hexlify(public_key.exportKey(format='DER')).decode('ascii'))
+
+    def sign_transaction(self, sender, recipient, amount):
+        signer = PKCS1_v1_5.new(RSA.importKey(binascii.unhexlify(self.private_key)))
+        h = SHA256.new((str(sender)+str(recipient)+str(amount))).encode('utf8')
+        signature = signer.sign(h)
+        return binascii.hexlify(signature).decode('ascii')
