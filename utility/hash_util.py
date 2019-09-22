c16b0bfbf7e1e5dee439198d4f45d3ec741f4dcf
Mwami Se-lunani <mwamiselunani@gmail.com>
2019-09-22 14:15:26 +0300

    creating a friendly ui

--- utility/hash_util.py
+++ utility/hash_util.py
@@ -7,7 +7,7 @@ def hash_string_256(string):
     """Create a SHA256 hash for a given input string.
 
     Arguments:
-        :string: The string which should be hashed.
+        :string: The string  which should be hashed.
     """
     return hl.sha256(string).hexdigest()
 
